bd9742a61b315c069a43b9e9f8068a07f311c94c
KanarekLife <kanareklife@gmail.com>
2020-12-06 12:18:58 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static int CountWhereAnyoneAnsweredYes(string content)
+        {
+            var temp = content.Split('\n');
+            var results = new List<int>();
+            var buffer = new Dictionary<char, bool>();
+            foreach (var line in temp.Append("\n"))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    results.Add(buffer.Count(x=>x.Value));
+                    buffer.Clear();
+                }
+                else
+                {
+                    foreach (var chr in line)
+                    {
+                        buffer[chr] = true;
+                    }
+                }
+            }
+
+            return results.Sum();
+        }
+
+        static int CountWhereEveryoneAnsweredYes(string content)
+        {
+            var temp = content.Split('\n');
+            var results = new List<int>();
+            var buffer = new Dictionary<char, int>();
+            var n = 0;
+            foreach (var line in temp.Append("\n"))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    results.Add(buffer.Count(x=>x.Value == n));
+                    buffer.Clear();
+                    n = 0;
+                }
+                else
+                {
+                    n++;
+                    foreach (var chr in line)
+                    {
+                        buffer[chr] = buffer.ContainsKey(chr) ? buffer[chr]+1 : 1;
+                    }
+                }
+            }
+
+            return results.Sum();
+        }
+        
+        static void Main(string[] args)
+        {
+            var content = File.ReadAllText("input.txt");
+            Console.WriteLine(CountWhereAnyoneAnsweredYes(content));
+            Console.WriteLine(CountWhereEveryoneAnsweredYes(content));
+        }
+    }
+}
